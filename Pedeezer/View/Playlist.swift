193185c46e7057504fa6ac29aaa1d279d7c8d60c
pedroclericuzi <pvbc@cin.ufpe.br>
2018-10-16 02:03:11 -0300

    Ajustando view

--- Pedeezer/View/Playlist.swift
+++ Pedeezer/View/Playlist.swift
@@ -9,29 +9,41 @@
 import UIKit
 import Foundation
 
-class Playlist : UITableViewController {
+class Playlist : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var menuTableView:UITableView!
     var musicas:[Musicas] = []
     let downloadImagem = DownloadImagem()
     let listaMusicas = ListaMusicas()
     var imagem: UIImageView?
     var nomeBanda:UILabel?
     var linkBanda:UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         navigationController?.navigationBar.topItem?.title = "\(arrArtista[0].nome)"
         self.viewDetalhes()
-        //self.mTableView()
+        self.mTableView()
         self.listaMusicas.gettingCountMusicas(i: 1){(output) in
             self.listaMusicas.getMusicasJSON(artista: 1, qtd: output) { (output2) in
-                //print(output2.count)
                 self.musicas = output2
                 DispatchQueue.main.async {
-                    self.tableView.reloadData()
+                    self.menuTableView!.reloadData()
                 }
                 
             }
         }
+    }
+    
+    func mTableView() {
+        let screenSize: CGRect = UIScreen.main.bounds
+        let navBarHeight = (navigationController?.navigationBar.frame.size.height)! + 20
+        self.menuTableView = UITableView(frame: CGRect(x: 0, y: (screenSize.height * 0.15)+navBarHeight, width: screenSize.width, height: (screenSize.height * 0.85)-navBarHeight))
+        self.menuTableView.delegate = self
+        self.menuTableView.dataSource = self
+        self.view.addSubview(menuTableView)
+        
     }
     
     func viewDetalhes() -> Void {
@@ -56,7 +68,7 @@ class Playlist : UITableViewController {
         self.nomeBanda = UILabel(frame: CGRect(x: (screenSize.width * 0.04) + (myViewHeight*0.5) + 5,
                                                y: navBarHeight + (myViewHeight * 0.25),
                                                width: screenSize.width * 0.6,
-                                               height: 17))
+                                               height: 18))
         self.nomeBanda?.font = UIFont(name: "Halvetica", size: 17)
         self.nomeBanda?.text = "Banda: \(arrArtista[0].nome)"
         self.nomeBanda?.textColor = UIColor.black
@@ -65,7 +77,7 @@ class Playlist : UITableViewController {
         self.linkBanda = UILabel(frame: CGRect(x: (screenSize.width * 0.04) + (myViewHeight*0.5) + 5,
                                                y: navBarHeight + (myViewHeight * 0.25) + 20,
                                                width: screenSize.width * 0.6,
-                                               height: 17))
+                                               height: 18))
         self.linkBanda?.font = UIFont(name: "Halvetica", size: 17)
         self.linkBanda?.text = "Ir para o perfil da banda"
         self.linkBanda?.textColor = UIColor.blue
@@ -76,27 +88,21 @@ class Playlist : UITableViewController {
         
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return musicas.count;
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: "Teste")
         cell.textLabel?.text = musicas[indexPath.row].nome
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //arrArtista = [dados[indexPath.row]]
         //let playlist = Playlist()
         //self.navigationController?.pushViewController(playlist, animated: false)
     }
-    
-    /*func mTableView() {
-        let screenSize: CGRect = UIScreen.main.bounds
-        let navBarHeight = (navigationController?.navigationBar.frame.size.height)! + 20
-        self.tableView = UITableView(frame: CGRect(x: 0, y: navBarHeight, width: screenSize.width, height: screenSize.height * 0.85))
-    }*/
     
     @objc
     func funcaoClique(sender:UITapGestureRecognizer) {
--- Pedeezer/View/ViewController.swift
+++ Pedeezer/View/ViewController.swift
@@ -9,14 +9,20 @@
 import UIKit
 
 var arrArtista:[Artistas] = []
-class ViewController: UITableViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var dados:[Artistas] = []
     let listaArtistas = ListaArtistas()
-    
+    var menuTableView:UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         navigationController?.navigationBar.topItem?.title = "Choose your singer"
+        let screenSize: CGRect = UIScreen.main.bounds
+        self.menuTableView = UITableView(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height))
+        self.menuTableView.delegate = self
+        self.menuTableView.dataSource = self
+        self.view.addSubview(menuTableView)
+        
         DispatchQueue.global(qos: .background).async {
             for i in 0 ..< 10 {
                 self.listaArtistas.parsingJSON(i: i) {(output) in
@@ -24,24 +30,24 @@ class ViewController: UITableViewController {
                     self.dados.append(Artistas(id: output[0].id, nome: output[0].nome, imagemUrl: output[0].imagemUrl, paginaUrl: output[0].paginaUrl))
                     //self.dados.append(output[0].nome)
                     DispatchQueue.main.async {
-                        self.tableView.reloadData()
+                        self.menuTableView!.reloadData()
                     }
                 }
             }
         }
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dados.count;
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: "Teste")
         cell.textLabel?.text = dados[indexPath.row].nome
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         arrArtista = [dados[indexPath.row]]
         let playlist = Playlist()
         self.navigationController?.pushViewController(playlist, animated: false)
